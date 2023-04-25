284b06354685304df1ad697b73af11800ef48d3a
mvallestlcc <mvalle.stlcc@gmail.com>
2023-04-25 08:23:13 -0600

    25/4

--- rrhh/urls.py
+++ rrhh/urls.py
@@ -7,8 +7,8 @@ urlpatterns = [
       # path('rrhh/expediente/index',views.expediente_index, name='expediente_index'),
       # path('rrhh/expediente/consulta',views.expediente_index2, name='expediente_index2'),
       # path('rrhh/expediente/nombres',views.expediente_index3, name='expediente_index3'),
-       path('rrhh/departamentos/index',departamentoListar.as_view(), name='departamentos_listar'),
-       path('rrhh/departamentos/agregar',departamentoAgregar.as_view(), name='departamentos_agregar'),
-       path('rrhh/departamentos/editar/<int:pk>/',departamentosEditar.as_view(), name='departamentos_editar'),   
-       path('rrhh/departamentos/eliminar/<int:pk>/',departamentosEliminar.as_view(), name='departamentos_eliminar'),        
+       path('departamentos/index',departamentoListar.as_view(), name='departamentos_listar'),
+       path('departamentos/agregar',departamentoAgregar.as_view(), name='departamentos_agregar'),
+       path('departamentos/editar/<int:pk>/',departamentosEditar.as_view(), name='departamentos_editar'),   
+       path('departamentos/eliminar/<int:pk>/',departamentosEliminar.as_view(), name='departamentos_eliminar'),        
   ]
--- ujcv2023/urls.py
+++ ujcv2023/urls.py
@@ -21,7 +21,7 @@ from django.contrib.auth import views as auth_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('rrhh.urls')),  
+    path('rrhh/', include('rrhh.urls')),  
     path('login/', auth_views.LoginView.as_view(template_name='base/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='base/index.html'), name='logout'),      
 
